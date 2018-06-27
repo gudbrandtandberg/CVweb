3f3a426df1e04e70b57dedea5784b4c89d7f841b
gudbrandtandberg <gudbrandduff@gmail.com>
2018-06-27 11:45:10 -0700

    web revamp more or less working

--- README.md
+++ README.md
@@ -12,17 +12,10 @@ to start the webserver on `localhost:5000`.
 
 If you do not have ChessVision installed, set the line `local = False` in `main.py` and start the server. __Note:__ this will only work when the official ChessVision server is running.
 
-### Image upload
-- https://github.com/blueimp/JavaScript-Load-Image
-
-
 ### Cropping: 
-- https://codepen.io/nakome/pen/vmKwQg
-- https://github.com/jseidelin/pixastic
-- https://foliotek.github.io/Croppie/
-- http://picturecut.tuyoshi.com.br/
-- https://github.com/fengyuanchen/cropper
-- http://odyniec.net/projects/imgareaselect/
+
+- [cropper](https://github.com/fengyuanchen/cropper)
+- [jquery-cropper](https://github.com/fengyuanchen/jquery-cropper)
 
 ### Chess:
-- http://chessboardjs.com/docs
+- [chessboardjs](http://chessboardjs.com/docs)
--- static/js/main.js
+++ static/js/main.js
@@ -1,143 +1,259 @@
-var board, cropper, cropperOptions;
+// 
+// main.js -- all the chessvision website stuff
+//
 
+// global variablaes
+var board, cropper, cropperOptions;
+var input, canvas, context, endpoint;
+
+// initialize variables
 var init = function() {
 
-    $("#board-state-pane").hide()
-    $("#edit-analyze-pane").hide()
-    //$("#image-preview").hide()
-    //$("#board").hide()
-    //$("#preview-container").show()
-    $("#board-container").hide()
-
-    cfg = {position: "8/8/8/8/8/8/8/8 w KQkq -",
-           dropOffBoard: "trash",
-           orientation: "white",
-           sparePieces: false,
-           showErrors: "console"
-        }
-
-    // The displayed chessboard (chessboard.js)
-    board = ChessBoard('board', cfg);
+    // Initialize chessboard (chessboard.js)
+    board = ChessBoard( 'board', {position: "8/8/8/8/8/8/8/8 w KQkq -",
+                        dropOffBoard: "trash",
+                        orientation: "white",
+                        sparePieces: false,
+                        showErrors: "console"
+                        });
     $(window).resize(board.resize);
 
+    // Initialize cropper (cropper.js)
     cropperOptions = { dragMode: "move",
+                viewMode: 3,
                 cropBoxMovable: false,
                 cropBoxResizable: false,
                 autoCropArea: 1.0,
+                aspectRatio: 1.0,
                 background: true,
                 guides: true,
                 center: false,
                 highlight: true,
                 modal: true,
+                toggleDragModeOnDblclick: false,
                 ready: function(event) {
-                    console.log("cropper ready")
+                    
                 },
                 crop: function(event) {
-                    console.log("cropping");
+                    
                   }
                 }
+    $("#image-preview").cropper(cropperOptions);
 
+    // Hook up event handlers
+    $("#upload-form").submit(extractBoard)      
+    document.getElementById("image-input").onchange = imageInputChanged
 
-    var endpoint = document.getElementById("endpoint").innerHTML
+} // end init
+
+// Fires every time a new file is selected (or file-choosing is cancelled)
+var imageInputChanged = function() {
+
+    canvas = document.getElementById("image-preview");
+    context = canvas.getContext('2d');
+    var reader = new FileReader();
+    
+    reader.addEventListener("loadend", function(arg) {
+
+      var src_image = new Image();
+      
+      src_image.onload = function() {
+        
+        canvas.height = src_image.height;
+        canvas.width = src_image.width;
+        context.drawImage(src_image, 0, 0);
+        
+        cropper = $("#image-preview").data("cropper");
+        cropper.replace(this.src)
+      }
+
+      src_image.src = this.result;
+      
+    });
+    if (this.files) {
+        reader.readAsDataURL(this.files[0]);
+        $("#board-container").hide()
+        $("#preview-container").show()
+        $("#board-state-pane").show()
+    } else {
+        alert("no file chosen")
+    }
+};
+
+// Package image and metadata in a formdata object and send to server
+var extractBoard = function(event) {
+    event.preventDefault()
+
+    endpoint = document.getElementById("endpoint").innerHTML
     
     var cv_algo_url = endpoint + "cv_algo/"
-    var feedback_url = endpoint + "feedback/"
-    var analyze_url = endpoint + "analyze/"
 
-    $("#upload-form").submit(function(event) {
-        event.preventDefault()
-
-        //var file = document.getElementById("image-input").files[0]
-        //cropper = $("#image-input").data('cropper');
-        //blob = cropper.getCroppedCanvas().toBlob()
-
-        dataURL = cropper.getCroppedCanvas().toDataURL('image/jpeg')
-        
-        var blobBin = atob(dataURL.split(',')[1]);
-        var array = [];
-        for(var i = 0; i < blobBin.length; i++) {
-            array.push(blobBin.charCodeAt(i));
-        }
-        var file=new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
-        var formData = new FormData();
-        formData.append("file", file);
-
-        // TODO input check, crop. etc.
+    dataURL = cropper.getCroppedCanvas().toDataURL('image/jpeg')
     
+    var blobBin = atob(dataURL.split(',')[1]);
+    var array = [];
+    for (var i = 0; i < blobBin.length; i++) {
+        array.push(blobBin.charCodeAt(i));
+    }
+    var file = new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+    flip = document.getElementById("reversed-input").checked ? "true" : "false"
 
-        $.ajax({
-            url: cv_algo_url,
-            method: "POST", 
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function(data) {
-                //parse data = {FEN: "...", id: "..."}
-                res = JSON.parse(data)
-                
-                if (res.error == "false") {
-                    $("#board-container").show()
-                    $("#preview-container").hide()
-                    
-                    $("#analyze-edit-pane").show()
-                    setFEN(res.FEN)
-                    document.getElementById("raw-id-input").value = res.id
-                    
-                } else {
-                    console.log(res)
-                }
+    var formData = new FormData();
+    formData.append("file", file);
+    formData.append("flip", flip);
+
+    $.ajax({
+        url: cv_algo_url,
+        method: "POST", 
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: uploadSuccess,
+        error: function(xmlHttpRequest, textStatus, errorThrown) {
+            if (xmlHttpRequest.readyState == 0 || xmlHttpRequest.status == 0) {
+                alert("Connection to ChessVision server failed. It is probably sleeping..")
+                return
+            } else {
+                alert(textStatus)
+            }
+        }
+    })
+}
+
+var uploadSuccess = function(data) {
+    //parse data = {FEN: "...", id: "..."}
+    res = JSON.parse(data)
+    
+    if (res.error == "false") {
+        $("#board-container").show()
+        board.resize()
+        $("#preview-container").hide()
+        $("#edit-analyze-pane").show()
+        setFEN(res.FEN)
+        document.getElementById("raw-id-input").value = res.id
+        
+    } else {
+        console.log(res)
+    }
+}
+
+$("#edit-btn").on("click", function(e) {
+    
+    var orientation = document.getElementById("reversed-input").checked ? "black" : "white"
+    var pos = board.position("fen")
+    board = ChessBoard( 'board', {
+    position: pos,
+    orientation: orientation,
+    dropOffBoard: "trash",
+    sparePieces: true,
+    });
+    //$(window).resize(board.resize);
+
+    $("#feedback-pane").show()
+    $("#submit-pane").hide()
+    $("#edit-analyze-pane").hide()
+})
+
+$("#feedback-form").submit(function(event) {
+    event.preventDefault()
+    var feedback_url = endpoint + "feedback/"
+    var position = board.position()
+    var flip = document.getElementById("reversed-input").checked ? "true" : "false"
+
+    var formData = new FormData(this);
+    formData.append("position", JSON.stringify(position))
+    formData.append("flip", flip)
+
+    $.ajax({
+        url: feedback_url,
+        method: "POST",
+        data: formData, 
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(data) {
+            res = JSON.parse(data)
+            $("#feedback-pane").hide()
+            var pos = board.position("fen")
+            var orientation = document.getElementById("reversed-input").checked ? "black" : "white"
+            board = ChessBoard( 'board', {
+                position: pos,
+                orientation: orientation,
+                sparePieces: false,
+                });
+            $("#submit-pane").show()
+            $("#edit-analyze-pane").show()
+
+            if (res.success == "true") {
+                alert("Thanks for your feedback!")
+            } else {
+                alert("Something went wrong, your feedback was not taken into consideration")
+                }           
             },
-            error: function(xmlHttpRequest, textStatus, errorThrown) {
-                if (xmlHttpRequest.readyState == 0 || xmlHttpRequest.status == 0) {
-                    alert("Connection to ChessVision server failed. It is probably sleeping..")
-                    return
-                } else {
-                    alert(textStatus)
-                }
+        error: function(data) {
+            alert("Feedback-ajax failed..")
+            console.log(data)
             }
         })
     })
 
-      var input, canvas, context, output;
-      input = document.getElementById("image-input");
-      canvas = document.getElementById("image-preview");
-      context = canvas.getContext('2d');
-      output = document.getElementById("output");
-      
-      
-      input.onchange = function() {
-        
-        $("#preview-container").show()
-        var reader = new FileReader();
-        
-        reader.addEventListener("loadend", function(arg) {
+$("#analyze-btn").on("click", function() {
+    
+    var analyze_url = endpoint + "analyze/"
+    var formData = new FormData(this);
 
-          var src_image = new Image();
-          
-          src_image.onload = function() {
-            
-            canvas.height = src_image.height;
-            canvas.width = src_image.width;
-            context.drawImage(src_image, 0, 0);
-            
-            $("#image-preview").cropper(cropperOptions);
-            cropper = $("#image-preview").data("cropper");
+    // get valid fen from board + input tags.
+    var fen = board.fen()
+    fen = expandFen(fen)
 
-            cropper.replace(this.src)
-          }
+    formData.append("FEN", fen)
 
-          src_image.src = this.result;
+    $.ajax({
+        url: analyze_url,
+        method: "POST",
+        data: formData, 
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(data) {
+            res = JSON.parse(data)
+            if (res.success == "false") {
+                alert("Analysis failed..")
+                return
+            }
+            var bestMove = res.bestMove
+            if (bestMove.length == 5) {
+                alert("strange move!")
+            }
+            src = bestMove.substr(0, 2)
+            dst = bestMove.substr(2, 2)
+            move = src.concat("-", dst)
+            board.move(move)
+            },
+        error: function(data) {
+            alert("error")
+            console.log(data)
+            }
+        })
+})
+    
+var expandFen = function(fen) {
+    var move = document.querySelector('input[name="move"]:checked').value;
+    var castle = "-"
+    var ep = "-"
+    var halfmove = "0"
+    var fullmove = "1"
+    var sep = " "
+    var toAdd = [move, castle, ep, halfmove, fullmove]
 
-          
-        });
+    for (var i=0; i < toAdd.length; i++) {
+        fen += sep
+        fen += toAdd[i]
+    }
 
-        reader.readAsDataURL(this.files[0]);
-        $("#board-state-pane").show()
-        $("#board-container").hide()
-      };
-
-} // end init
+    return fen
+}
 
 var setFEN = function(fen) {
     
